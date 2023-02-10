3e250fc2237ec57dc8b1eec87b05305f310130ce
Lucas Borza <112983053+LucasBorzaPRA@users.noreply.github.com>
2023-02-10 21:43:45 +0000

    commit

--- myrg.tf
+++ myrg.tf
@@ -1,9 +1,9 @@
 resource "azurerm_resource_group" "example" {
   name     = "example1-for-lucas"
-  location = "East US"
+  location = "East US 2"
 }
 
 resource "azurerm_resource_group" "example2" {
   name     = "example2-for-lucas"
-  location = "East US"
+  location = "East US 2"
 }
